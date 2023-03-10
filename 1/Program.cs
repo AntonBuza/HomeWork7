b2ef2ad91fd22c5a6e0fc2b34031e9299fc0c8d7
Anton Buza <Buzaant@mail.ru>
2023-03-10 12:41:31 +0700

    HW7 done

---
+++ 1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] GenerateMatrix(int str, int column)
+{
+    double[,] matrix = new double[str, column];
+    Random rand =new Random();
+    for(int i = 0; i < str; i++)
+    {
+        for(int j = 0; j < column; j++)
+        {
+            matrix[i,j] = rand.Next(1, 10)+rand.NextDouble();
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+System.Console.WriteLine("Введите количество строк матрицы:");
+int str = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите количесвто столбцов матрицы:");
+int column = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = GenerateMatrix (str, column);
+PrintMatrix (matrix);
---
+++ 2/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение
+// этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] CreateMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 11);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+
+        }
+        System.Console.WriteLine();
+    }
+
+}
+
+void Searh (int[,] matrix)
+{
+    System.Console.WriteLine("Введите строку:");
+    int m = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine("Введите столбец:");
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (m <= matrix.GetLength(0) && n <= matrix.GetLength(1))
+    {
+        System.Console.Write(matrix[m-1,n-1]);
+    }
+    else 
+    {
+        System.Console.Write("Такого числа в массиве нет!");
+    }
+}
+
+int[,] matrix = CreateMatrix (5,5);
+PrintMatrix (matrix);
+Searh (matrix);
